a99e8e2d4bf38b2631e76ee5b69d637b86d17a4d
davidmp <mamanipari@gmail.com>
2022-06-12 19:44:56 -0500

    Completado Calculadora P2

--- prueba/lib/main.dart
+++ prueba/lib/main.dart
@@ -2,44 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 import 'package:google_fonts/google_fonts.dart';
 import './com/ButtonComp.dart';
+
 void main() {
   runApp(const CalcApp());
 }
+
 class CalcApp extends StatefulWidget {
   const CalcApp({Key key}) : super(key: key);
   @override
   CalcAppState createState() => CalcAppState();
 }
 
-
 class CalcAppState extends State<CalcApp> {
   String _expression = '';
-  String valorAnt='';
-  String operador='';
+  String valorAnt = '';
+  String operador = '';
 
   void numClick(String text) {
     setState(() => _expression += text);
   }
+
   void opeClick(String text) {
-    setState((){
-      valorAnt=_expression;
-      operador=text;
-      _expression= '';
-    }
-    );
+    setState(() {
+      valorAnt = _expression;
+      operador = text;
+      _expression = '';
+    });
   }
 
-
-  void resultOperacion(String text){
-    setState((){
-      switch(operador){
-        case "/": {
-          _expression=(int.parse(valorAnt)/int.parse(_expression)).toString();
-        } break;
-        case "*" :{  } break;
+  void resultOperacion(String text) {
+    setState(() {
+      switch (operador) {
+        case "/":
+          _expression =
+              (int.parse(valorAnt) / int.parse(_expression)).toString();
+          break;
+        case "*":
+          _expression =
+              (int.parse(valorAnt) * int.parse(_expression)).toString();
+          break;
+        case "+":
+          _expression =
+              (int.parse(valorAnt) + int.parse(_expression)).toString();
+          break;
+        case "-":
+          _expression =
+              (int.parse(valorAnt) - int.parse(_expression)).toString();
+          break;
+        case "%":
+          _expression =
+              (int.parse(valorAnt) % int.parse(_expression)).toString();
+          break;
       }
-      }
-    );
+    });
   }
 
   void clear(String text) {
@@ -201,17 +216,10 @@ class CalcAppState extends State<CalcApp> {
                   ),
                 ],
               ),
-
-
-
             ],
           ),
         ),
       ),
     );
   }
-
-
 }
-
-
